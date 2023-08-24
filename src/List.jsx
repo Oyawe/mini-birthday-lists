09014a4cd89943a8ae553492cd317cf6c1c778ea
Oyawe <oyawegoodness@gmail.com>
2023-08-24 00:29:52 -0700

    little glitch

--- src/List.jsx
+++ src/List.jsx
@@ -5,7 +5,15 @@ const List = ({ people }) => {
   return (
     <section>
       {people.map((person) => {
-        return <Person key={person.id} {...person} />;
+        // return <Person key={person.id} {...person} />;
+        return (
+          <Person
+            key={person.id}
+            image={person.image}
+            name={person.name}
+            age={person.age}
+          />
+        );
       })}
     </section>
   );
